15597f65c3e54ae3c8d68faaa00bfe4c50a3c1f9
imazahmedlodi <73024346+imazahmedlodi@users.noreply.github.com>
2020-11-05 17:48:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var fixedrect,movingrect
+function setup() {
+  createCanvas(800,400);
+  movingrect=createSprite(400, 200, 80, 30);
+ movingrect.shapeColor="red"
+ fixedrect=createSprite(200, 300, 50, 50);
+ fixedrect.shapeColor="red"
+}
+
+function draw() {
+  background(255,255,255);  
+  movingrect.x=World.mouseX
+  movingrect.y=World.mouseY
+  if (movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2 && fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2 &&
+    movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2 && fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2 ){
+    movingrect.shapeColor="green"
+    fixedrect.shapeColor="green" 
+  }
+  else
+{
+  movingrect.shapeColor="red"
+  fixedrect.shapeColor="red"
+}
+  drawSprites();
+}
